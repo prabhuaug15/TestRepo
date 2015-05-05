6b1bab5b3bcab087a954b424704db53381fc885a
tkmagesh <tkmagesh77@gmail.com>
2015-05-05 14:52:32 +0530

    Service

--- Day-02/01-BugTracker/utils.js
+++ Day-02/01-BugTracker/utils.js
@@ -4,8 +4,10 @@ angular.module('utils',[])
 
          function Bug(defautls){
             defaults = defautls || {};
+            this.id = defautls.id || Date.now().toString();
             this.title = defaults.title || '';
             this.isClosed = defaults.isClosed || defaultClosedStatus;
+            this.createdAt = defautls.createdAt || new Date();
         }
         Bug.prototype.toggle = function(){
             this.isClosed = !this.isClosed;
